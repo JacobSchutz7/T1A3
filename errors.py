66d7d450b5ea3c759b633b5e085c91a87c57512f
JacobSchutz7 <jacobschutz97@gmail.com>
2024-06-28 16:41:23 +0930

    added more error handling

--- errors.py
+++ errors.py
@@ -1,3 +1,5 @@
+from data import *
+
 # Helper function for error handling
 def get_valid_name(prompt):
 	while True:
@@ -9,11 +11,22 @@ def get_valid_name(prompt):
 		except ValueError as e:
 			print(f"Error: {e}")
 
-def validate_entry(prompt, dictionary, exists):
+def validate_entry(student_ID, class_title):
+	# Get student ID and check if that student exists
+	if student_ID not in students:
+		print(f"Student ID '{student_ID}' not found.")
+		return False
+	# Get class title and check if that class exists
+	if class_title not in list_of_classes:
+		print(f"class '{class_title}' not found.")
+		return False
+	return True
+
+def validate_student_removal(student_ID):
 	try: 
-		key = input(prompt)
-		if (exists and key in dictionary) or (not exists and key not in dictionary):
-			raise ValueError(f"Entry {'already' if exists else 'does not'} exist.")
-		return key
-	except ValueError as e:
-		print(f"Error: {e}")
+		if student_ID in students:
+			students.pop(student_ID)
+			print("Student removed.")
+	except KeyError:
+		print(f"Error: Student ID {student_ID} not found.")
+
--- methods.py
+++ methods.py
@@ -10,7 +10,7 @@ def add_student():
 	count = len(students)
 	students[count] = name
 
-# Add a new class e.g. "History 101"
+# Add a new class e.g. "History"
 def add_class():
 	title = validate_entry("Enter class: ", list_of_classes, 1)
 	list_of_classes[title] = (Classes(title))
@@ -25,13 +25,7 @@ def add_student_to_class():
 	student_ID = int(input("Enter Student ID: "))
 	class_title = input("Enter class title: ")
 	# Get student ID and check if that student exists
-	if student_ID not in students:
-		print(f"Student ID '{student_ID}' not found.")
-		return
-	# Get class title and check if that class exists
-	if class_title not in list_of_classes:
-		print(f"class '{class_title}' not found.")
-		return
+	validate_entry(student_ID, class_title)
 	# Add student to the class if inputs have been successfully validated
 	student = students.get(student_ID)
 	class_object = list_of_classes[class_title]
@@ -40,18 +34,31 @@ def add_student_to_class():
 # Remove a student from cohort
 def remove_student():
 	student_ID = int(input("Enter student ID: "))
-	try: 
-		if student_ID in students:
-			students.pop(student_ID)
-			print("Student removed.")
-	except KeyError:
-		print(f"Error: Student ID {student_ID} not found.")
+	validate_student_removal(student_ID)
 
 # Remove a student from a class
-def remove_student_from_class(student_ID, class_title):
-	class_object = list_of_classes[class_title]
-	class_object.students.pop(student_ID)
+def remove_student_from_class():
+	try:
+		student_ID = int(input("Enter student ID: "))
+		class_title = input("Enter class title: ")
+		if validate_entry(student_ID, class_title):
+			class_object = list_of_classes[class_title]
+			if student_ID in class_object.students:
+				class_object.students.pop(student_ID)
+			else: 
+				print("Student not in found in class.")
+		else: 
+			print("Student not found in class.")
+	except ValueError as e:
+		print(f"Error: {e}")
 
 # Print all students in a class
-def print_students_in_class(class_title):
-	print(list_of_classes.get(class_title).students)
+def print_students_in_class():
+	try:
+		class_title = input("Enter class title: ")
+		if class_title in list_of_classes:
+			print(list_of_classes.get(class_title).students)
+		else:
+			print("Class not found.")
+	except Exception as e:
+		print("Error: {e}")
